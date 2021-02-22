02625a792d677e0add7445afbbb258fb149aa4c6
stephcaldwell <stephcaldwell97@gmail.com>
2021-02-22 13:06:40 -0600

    First JS exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,48 @@
+//1.
+console.log(42);
+//2.
+// Five main data types are: numbers, strings, boolean, null and undefined.
+//3.
+//The 6 operator symbols are + addition, - subtraction, * multiply, / divide, ** is for exponent, % modulus or remainder.
+
+//4.
+console.log(5%2);
+console.log(5%4);
+
+//5.
+console.log("I’M MARY POPPINS, Y’ALL!");
+
+//6.
+
+console.log("Wanda" + "Vision");
+
+//7.
+console.log("Stephanie" .length + "Caldwell" .length);
+
+//8.
+console.log("Avengers: Endgame" [16]);
+
+//BONUS 
+
+//1.
+console.log("\/");
+
+//2.
+
+console.log(5**4-9);
+
+//3.
+
+console.log("IRON" .length - "am" .length - "man" .length + "i" .length);
+
+//4.
+
+//CAP” “VISION” “HAWKEYE” “NICK FURY” “GROOT” “DOCTOR STRANGE” “THOR” “STAR-LORD
+
+console.log("CAP" [1] + "VISION" [0] + "HAWKEYE" [4] + "NICK FURY" [0]+ "GROOT" [0] + "DOCTOR STRANGE" [13] + "THOR" [3] + "STAR-LORD" [0]);
+
+//5.
+var m=10;
+var c=5;
+var u = m + c;
+console.log( "The total is "  + u);
