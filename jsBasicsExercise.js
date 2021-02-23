c89b4350328e40ca37e2ac2720f14f6653a5bd12
stephcaldwell <stephcaldwell97@gmail.com>
2021-02-22 18:49:56 -0600

    Exercise with corrections

--- jsBasicsExercise.js
+++ jsBasicsExercise.js
@@ -3,14 +3,14 @@ console.log(42);
 //2.
 // Five main data types are: numbers, strings, boolean, null and undefined.
 //3.
-//The 6 operator symbols are + addition, - subtraction, * multiply, / divide, ** is for exponent, % modulus or remainder.
+//The 6 operator symbols are + addition, - subtraction, * multiply, / divide, ** exponent, % modulus or remainder.
 
 //4.
 console.log(5%2);
 console.log(5%4);
 
 //5.
-console.log("I’M MARY POPPINS, Y’ALL!");
+console.log('I\’M MARY POPPINS, Y\’ALL!');
 
 //6.
 
@@ -25,7 +25,7 @@ console.log("Avengers: Endgame" [16]);
 //BONUS 
 
 //1.
-console.log("\/");
+console.log("\\");
 
 //2.
 
@@ -33,16 +33,30 @@ console.log(5**4-9);
 
 //3.
 
-console.log("IRON" .length - "am" .length - "man" .length + "i" .length);
+// my answer (didnt follow directions exactly
+// console.log("IRON" .length - "am" .length - "man" .length + "i" .length);
 
+// console.log("I".length % "am" .length - "iron".length % "man".length);
+console.log("I".length - "am" .length + "iron".length - "man".length);
+// console.log("I".length - "am" .length + ("iron".length %2 - "man".length));
 //4.
 
-//CAP” “VISION” “HAWKEYE” “NICK FURY” “GROOT” “DOCTOR STRANGE” “THOR” “STAR-LORD
+console.log("cap" [1]);
+console.log("vision" [0]);
+console.log("hawkeye" [4]);
+console.log("nick fury" [0]);
+console.log("groot" [0]);
+console.log("doctor strange" [13]);
+console.log("thor" [3]);
+console.log("star-lord" [0]);
 
-console.log("CAP" [1] + "VISION" [0] + "HAWKEYE" [4] + "NICK FURY" [0]+ "GROOT" [0] + "DOCTOR STRANGE" [13] + "THOR" [3] + "STAR-LORD" [0]);
+// Answer was correct for letters but needed to be vertical 
+//console.log("CAP" [1] + "VISION" [0] + "HAWKEYE" [4] + "NICK FURY" [0]+ "GROOT" [0] + "DOCTOR STRANGE" [13] + "THOR" [3] + "STAR-LORD" [0]);
+//console.log("CAP" [1] + "\n" + "VISION" [0] + "\n" + "HAWKEYE" [4] + "\n" + "NICK FURY" [0]+ "\n" + "GROOT" [0] + "\n" + "DOCTOR STRANGE" [13] + "\n" + "THOR" [3] + "\n" + "STAR-LORD" [0]);
+
 
 //5.
 var m=10;
 var c=5;
 var u = m + c;
-console.log( "The total is "  + u);
+console.log( "The value for U is "  + u);
